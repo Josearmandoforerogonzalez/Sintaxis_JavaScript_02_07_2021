bb6fef8fd98e4f63dfd1b51a16b576bc2e20ce81
Josearmandoforerogonzalez <joseforero0310@gmail.com>
2021-07-07 21:05:43 -0500

    clase del 02 de julio sintaxis

---
+++ main.js
@@ -0,0 +1,112 @@
+
+nombreString = "jose";
+edadNumber = 21;
+soyProfesorBolea = true;
+estudiantesAprobadosDiplomadoNull = null;
+cocerUndefined = undefined;
+hobbiesArray = ["Programar","Cocino", 3043640755, null];
+hobbiesArray["Soy profesor"] = soyProfesorBolena;
+
+direccionPersonalObjeted = {
+    direccion: "calle 30",
+    pais: "colombia",
+    mundo: "tierra",
+    0: "Hola soy el identificador numerico"
+};
+
+console.log(hobbiesArray);
+console.log(dirrecionPersonalObjecte);
+
+console.log(`Datos de la variable tipo Array:`,hobbiesArray["Soy profesor"], hobbiesArray[0]);
+console.log(`Datos de la variable tipo Object:`,dirrecionPersonalObjecte.pais, dirrecionPersonalObjecte[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+// num1 = 50;
+// num2 = 3;
+
+// tipoDeDato = typeof(num1);
+
+
+// console.log(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+
+
+
+
+
+
+// console.log(`%c
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `,"color:orange; font-family:cursive;");
+
+// console.log(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+// console.error(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+// console.info(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+// console.warn(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+// document.write(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+// alert(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+
+
+
+
+
+
+
+// console.log("El resultado de la suma es: "+suma);
+// console.log('El resultado de la suma es: '+suma);
+// console.log('El resultado de la suma es: ',suma);
+
+
+
+
+
+
+
+
+
+
+
+
